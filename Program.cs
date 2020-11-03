809275a0f1e96683e86d14c004503f6cf78956e3
janzen-aeva <jhouchen-wilder@aevapharmacy.com>
2020-11-03 10:34:57 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace LongestCommonPrefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LongCommonPrefix(new string[] { "flower", "flow", "flight" }));
+        }
+
+
+        static string LongCommonPrefix(string[] strs)
+        {
+            if (strs == null)
+            {
+                return "";
+            }
+            
+            if (strs.Length == 1)
+            {
+                return strs[0];
+            }
+
+            string prefix = "";
+            for (int i = 0; i < strs[0].Length; i++)
+            {
+                for (int j = 0; j < strs.Length; j++)
+                {
+                    if (strs[0][i] != strs[j][i])
+                    {
+                        return prefix;
+                    }
+                }
+
+                prefix += strs[0][i];
+            }
+            return prefix;
+        }
+    }
+}
